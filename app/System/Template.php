7d283cd378e84148e490eac1420589f9aa545501
Aleksey Peredelskiy <ap@logiceditor.com>
2024-02-05 10:34:02 +0300

    wip

--- app/System/Template.php
+++ app/System/Template.php
@@ -5,11 +5,17 @@ class Template
 {
   public function render($template, $data)
   {
+    // globals for template
     $DATA = $data;
+    $BODY = $template . '.php';
+    
     ob_start();
-    include APP_DIR . '/Template/' . $template . '.php';
+    include APP_DIR . '/Template/' . '_layout.php';
     $content = ob_get_contents();
     ob_end_clean();
+
+    unset($DATA);
+    unset($BODY);
 
     return new Response($content);
   }
